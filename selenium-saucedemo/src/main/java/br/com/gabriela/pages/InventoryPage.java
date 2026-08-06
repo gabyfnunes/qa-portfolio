9d0a4710c87215fc86f5b00174bdcce8d5fa40c5
Gabriela <gabrieladefreitasnunes@gmail.com>
2026-08-06 10:46:42 -0300

    feat: add login tests scenarios

--- selenium-saucedemo/src/main/java/br/com/gabriela/pages/InventoryPage.java
+++ selenium-saucedemo/src/main/java/br/com/gabriela/pages/InventoryPage.java
@@ -5,6 +5,12 @@ import org.openqa.selenium.By;
 public class InventoryPage extends BasePage {
 
     private final By title = By.className("title");
+    private static final String INVENTORY_URL =
+            "https://www.saucedemo.com/inventory.html";
+
+    public void open() {
+        open(INVENTORY_URL);
+    }
 
     public String getPageTitle() {
         return find(title).getText();
--- selenium-saucedemo/src/main/java/br/com/gabriela/pages/LoginPage.java
+++ selenium-saucedemo/src/main/java/br/com/gabriela/pages/LoginPage.java
@@ -2,6 +2,12 @@ package br.com.gabriela.pages;
 
 import br.com.gabriela.config.ConfigReader;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 
 public class LoginPage extends BasePage{
@@ -9,16 +15,40 @@ public class LoginPage extends BasePage{
     private final By usernameField = By.id("user-name");
     private final By passwordField = By.id("password");
     private final By loginButton = By.id("login-button");
+    private final By errorMessage = By.cssSelector("[data-test='error']");
+    private final By closeErrorMsgButton = By.cssSelector("[data-test='error-button']");
 
     public void open() {
         open(ConfigReader.getProperty("base.url"));
     }
 
-    public void login(String username, String password) {
+    public void inputCredentials (String username, String password){
         type(usernameField, username);
         type(passwordField, password);
+    }
+
+    public void login(String username, String password) {
+        inputCredentials(username, password);
         clickLoginButton(loginButton);
 
     }
+
+    public String getLoginErrorTxt (){
+        return find(errorMessage).getText();
+    }
+
+    public void closeErrorMessage (){
+        find(closeErrorMsgButton).click();
+    }
+
+    public boolean isErrorMessageDisplayed (){
+        return !driver.findElements(errorMessage).isEmpty();
+    }
+
+    public String getPasswordFieldType() {
+        return driver.findElement(passwordField).getAttribute("type");
+    }
+
+
 
 }
